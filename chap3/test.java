99419fd583ec4d4aa4cee75ad1110bf02a219007
madao33 <3282389858@qq.com>
2020-12-09 15:48:40 +0800

    change test.java

--- chap3/test.java
+++ chap3/test.java
@@ -1,12 +1,21 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class test{
     public static void main(String args[]){
-        int [ ] arr = new int[ 10 ];
-        int [ ] newArr = new int[ arr.length * 2];
-        for( int i = 0; i < arr.length; i++){
-            newArr[i] = arr[i];
-            System.out.println(arr[i]);
-        }
-        arr = newArr;
-
+        List<Integer> lst = new LinkedList<Integer>();
+        makeList1(lst, 10);
+        for(Integer x:lst)
+            System.out.println(x);
+        
     }
+
+    // 末端添加一些项目来构造一个List
+    public static void makeList1(List<Integer> lst, int N){
+        lst.clear();
+        for(int i = 0; i < N; i++)
+            lst.add(i);
+    }
+
+    // 
 }
